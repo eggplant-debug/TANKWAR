ab75449d921e3de25571c6aec4c31bd8bc6a1e35
eggplant-debug <18391458847@163.com>
2021-08-17 21:56:19 +0800

    # 1.增加了配置文件的写法，并同时将该写法改成单例设计模式，进行书写

--- src/TANK/PropertyMgr.java
+++ src/TANK/PropertyMgr.java
@@ -6,6 +6,23 @@ import java.util.Properties;
 public class PropertyMgr {
     static Properties props =new Properties();
 
+    private  PropertyMgr (){
+
+    }
+
+    /*
+    单例设计模式(内部类实现)，最好的一种之一。
+    由于内部实现类进行实现，不会有多余加载的问题
+    通过enum 类来实现，可以解决反射来使用方法的问题，其防止了反序列化的情况
+
+     */
+    private  static class PropertyMgrHolder{
+        private final static PropertyMgr INSTANCE= new PropertyMgr();
+    }
+    public static PropertyMgr getInstance(){
+        return PropertyMgrHolder.INSTANCE;
+    }
+
     static{
         try{
             props.load(PropertyMgr.class.getClassLoader().getResourceAsStream("config"));
